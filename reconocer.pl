053ddfa0d845c7a490230d4323548dc3d207fa3f
4meta5 <asinghchrony@protonmail.com>
2020-11-06 12:43:46 -0500

    fmt

--- reconocer.pl
+++ reconocer.pl
@@ -4,13 +4,13 @@
 */
 
 geometric([X,Y],Q) :-
-    Y is Q * X.
-geometric([X,Y|T], Q) :-
-    Y is Q * X,
-    geometric([Y|T], Q).
+    Y is Q*X.
+geometric([X,Y|T],Q) :-
+    geometric([X,Y],Q),
+    geometric([Y|T],Q).
 
 arithmetic([X,Y],Q) :-
-    Y is Q + X.
-arithmetic([X,Y|T], Q) :-
-    Y is Q + X,
-    arithmetic([Y|T], Q).
+    Y is Q+X.
+arithmetic([X,Y|T],Q) :-
+    arithmetic([X,Y],Q),
+    arithmetic([Y|T],Q).
