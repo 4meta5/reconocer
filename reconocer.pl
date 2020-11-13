60f9572955f20fc336959284136523684e614643
4meta5 <asinghchrony@protonmail.com>
2020-11-12 23:16:24 -0500

    linear homogenous equations of degree 2

--- reconocer.pl
+++ reconocer.pl
@@ -1,9 +1,78 @@
 /*
-* Recognize (some) Recurrence Relations
+* Recognize Integer Relations
 * arithmetic, geometric sequences
 * special sequences i.e. fibonacci numbers
 */
 :- use_module(library(clpfd)).
+
+/*Recognize Sequence from List of Integers*/
+known(L) :-
+    arithmetic(L,_);geometric(L,_).
+
+/* WARNING: only written for equations where B and C are negative (see input assumptions for linear)
+?- quadratic(1,1,2,R).
+R = [1.0, 2.0].
+?- quadratic(1,3,18,R).
+R = [3.0, 6.0].
+?- quadratic(1,3,10,R).
+R = [2.0, 5.0].
+*/
+quadratic(A,NegB,NegC,[X1,NegX2]) :-
+    B2 is NegB*NegB,
+    AC is (4 * A * NegC),
+    RS is B2 + AC,
+    SQ is sqrt(RS),
+    X1 is (SQ-NegB)/2,
+    NegX2 is (NegB+SQ)/2.
+/* Linear Homogenous Equation Degree 2
+* input equation a_n = (B)a_n-1 + (C)a_n-2 s.t. B,C are both positive integers
+* A0, A1 are initial conditions when n = 0,1 respectively
+* Result provides n-term formula: A_n = C1*X1^n + C2*NegX2^n
+?- linear([3,18],[2,15],R).
+R = [[3.0, 3.0], [6.0, -1.0]].
+?- linear([3,10],[5,10],R).
+R = [[2.0, 5.0], [5.0, 0.0]].
+*/
+linear([B, C],[A0, A1],[[X1,C1],[NegX2,C2]]) :-
+    quadratic(1,B,C,[X1,NegX2]),
+    C1 is ((A0*NegX2)+A1)/(X1+NegX2),
+    C2 is A0-C1.
+/* Solve system of linear equations for X,Y \in 1..10
+*  C1*X + C2*Y = S1
+*  D1*X + D2*Y = S2
+?- linear_eq(1,1,6,-3,1,2,X,Y).
+X = 1,
+Y = 5.
+?- linear_eq(2,1,5,-1,1,2,X,Y).
+X = 1,
+Y = 3.
+*/
+linear_eq(C1,C2,S1,D1,D2,S2,X,Y) :-
+    X in 1..10,Y in 1..10,
+    A1 #= X*C1,
+    A2 #= Y*C2,
+    S1 #= A1+A2,
+    B1 #= X*D1,
+    B2 #= Y*D2,
+    S2 #= B1+B2.
+
+/*Recognize simple linear recurrences*/
+linear_rec(L,_) :- length(L,N),N<3,!. %Cut to end recursion
+/*
+TODO FIX
+?- linear_rec([1,4,13,25],R).
+false.
+*/
+linear_rec([H1,H2,H3,H4|T],[B,C]) :-
+    /*solve for B, C from H3 = H1*B + H2*C, H4 = H2*B + H3*C*/
+    linear_eq(H1,H2,H3,H2,H3,H4,B,C),
+    linear_rec([H2,H3,H4|T],[B,C]).
+/*Introduce arity3 method for processing*/
+linear_rec([H1,H2|T],[B,C],[A0,A1]) :-
+    length([H1,H2|T],N),N>=4,
+    A0 is H1,
+    A1 is H2,
+    linear_rec([H1,H2|T],[B,C]).
 
 arithmetic([X,Y],Q) :-
     Q is Y-X.
@@ -17,28 +86,19 @@ geometric([X,Y|T],Q) :-
     geometric([X,Y],Q),
     geometric([Y|T],Q).
 
-/*TODO: arithmetico-geometric 
-* https://en.wikipedia.org/wiki/Arithmetico%E2%80%93geometric_sequence 
-*/
-
-/*Checks if the sequence can be recognized
-TODO: add special sequences*/
-known(L) :-
-    arithmetic(L,_);geometric(L,_).
-/*TODO: combine into one method based on which relationship is recognized
--->QUESTION: are there any sequences that are arithmetic and geometric?*/
+/*Nth Term Formulas*/
 nth_arithmetic([H0|Seq],N,R) :-
     arithmetic([H0|Seq],Q) -> X is N*Q, R is H0+X.
 nth_geometric([H0|Seq],N,R) :-
     geometric([H0|Seq],Q) -> X is Q**N, R is H0*X.
 
+/*Partial Sum Formulas*/
 partial_sums_arithmetic([H0|_],0,H0).
 partial_sums_arithmetic([H0|Seq],N,R) :-
     arithmetic([H0|Seq],Q),
     X is (N+1)*H0,
     Y is Q*N*(N+1)*0.5,
     R is X+Y.
-
 partial_sums_geometric([H0|_],0,H0).
 partial_sums_geometric([H0|Seq],N,R) :-
     geometric([H0|Seq],Q),
@@ -51,12 +111,12 @@ fact(X,R) :-
     X1 is X - 1,
     fact(X1, R1),
     R is R1*X.
-/*Binomial Coefficient; N choose R where order doesn't matter*/
+/*Binomial Coefficient, N choose R where order doesn't matter*/
 binomial_co(N,R,Result) :-
     permutation(N,R,R1),
     fact(R,R2),
     Result is R1/R2.
-/*Permutation Count; N choose R where order matters*/
+/*Permutation Count, N choose R where order matters*/
 permutation(N,R,Result) :-
     fact(N,R1),
     fact(N-R,R2),
@@ -88,7 +148,7 @@ fib(N,R) :-
     fib(N1,R1),
     fib(N2,R2),
     R is R1 + R2.
-/*Lucas Numbers, Fibonacci w different starting values*/
+/*Lucas Numbers (Fibonacci w different initial values)*/
 lucas(0,2) :- !.
 lucas(1,1) :- !.
 lucas(N,R) :-
@@ -98,9 +158,7 @@ lucas(N,R) :-
     lucas(N2,R2),
     R is R1 + R2.
 
-/*
-Pascal's Recurrence
-*/
+/*Pascal's Recurrence*/
 pascal(X,X,1).
 pascal(X,0,R) :-
     X>0 -> R is 0.
@@ -135,7 +193,6 @@ first_stir(P,K,Result) :-
     Result is ((P1*R1) + R2).
 
 /*SEQUENCES
-* TODO: meta-predicate to recognize any of them of length N
 * TODO: sequence extensions for 
 * (1)first stirling (2)second stirling (3)pascal
 */
